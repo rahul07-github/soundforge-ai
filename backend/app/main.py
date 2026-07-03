165dcb98556df9a6bfdc8a02fbd3f7130668ab06
Faheemkha615-stack <faheemkha.615@gmail.com>
2026-07-03 12:25:13 +0530

    music,py update

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+from backend.app.api.music import router as music_router
 from backend.app.api.lyrics import router as lyrics_router
 
 app = FastAPI()
 
 app.include_router(lyrics_router)
+app.include_router(music_router)
