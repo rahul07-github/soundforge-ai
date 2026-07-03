df8560c9d5934fb230be67c4e29552e7925c26f7
Shilpi Tamrakar <shilpitamrakar06@gmail.com>
2026-07-03 12:47:05 +0530

    auth setup

---
+++ backend/test_security.py
@@ -0,0 +1,9 @@
+from app.auth.security import hash_password, verify_password
+
+password = "mypassword123"
+
+hashed = hash_password(password)
+print("Hashed Password:", hashed)
+
+print("Verify Correct:", verify_password(password, hashed))
+print("Verify Wrong:", verify_password("wrongpassword", hashed))
